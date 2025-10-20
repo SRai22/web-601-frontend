4b8d4946a54cdd539948de420bbaede4e0ccea85
SRai22 <shravansomashekara.rai@gmail.com>
2025-10-19 23:28:13 -0700

    modal added

--- m2_week3_day2_homework/js/activities.js
+++ m2_week3_day2_homework/js/activities.js
@@ -1,29 +1,31 @@
 $(document).ready(function() {
     $("td").click(function () { //user select a table data cell
         var content = $(this).text(); //get content of cell
-        
-        if (content != "Not Available") { //check if content does not contain a particular string
+
+        if (content != "Not Available" && !$(this).is(':first-child')) { //check if content does not contain "Not Available" and not first column
             $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected
-            
+
             if ($(this).hasClass("tdhighlight")) { //check if selected cell has class
                 // Get the column index of the clicked cell
                 var columnIndex = $(this).index();
-                
+
                 // Get the cliff site name from the header row using eq() method
                 var cliffSite = $('thead th').eq(columnIndex).text();
-                
+
                 // Create display text with styled cliff site
                 var displayText = content + " at <span class='cliff-site'>" + cliffSite + "</span>";
-                
-                $('#displaySelected').css("visibility","visible"); //make display box visible
-                $('#displaySelected').css("margin-top","2em"); //add spaces above display box
+
                 $('#result').append("<p>"+displayText+"</p>"); //add child element with contents including cliff site
-                
-            } else { //if selected cell don't have class
+
+                // Show the modal
+                var modal = new bootstrap.Modal(document.getElementById('activitiesModal'));
+                modal.show();
+
+            } else { //if selected cell doesn't have class
                 // Get the column index and cliff site for removal
                 var columnIndex = $(this).index();
                 var cliffSite = $('thead th').eq(columnIndex).text();
-                
+
                 // Find and remove the specific activity item
                 $('#result p').each(function() {
                     var activityText = $(this).text();
@@ -33,10 +35,13 @@ $(document).ready(function() {
                         return false; // Break out of the loop
                     }
                 });
-                
-                if ($('#result').has('p').length == false) { //check if there are any child elements within parent
-                    $('#displaySelected').css("visibility","hidden"); //make display box hidden
-                    $('#displaySelected').css("margin-top","0"); //remove spaces above display box
+
+                // If no more selections, hide the modal if it's open
+                if ($('#result').has('p').length == false) {
+                    var modal = bootstrap.Modal.getInstance(document.getElementById('activitiesModal'));
+                    if (modal) {
+                        modal.hide();
+                    }
                 }
             }
         }
