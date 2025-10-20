aa945797340d835490779be0d586181132399faa
SRai22 <shravansomashekara.rai@gmail.com>
2025-10-19 23:32:18 -0700

    Clean-up

--- m2_week3_day2_homework/js/activities.js
+++ m2_week3_day2_homework/js/activities.js
@@ -1,42 +1,33 @@
 $(document).ready(function() {
-    $("td").click(function () { //user select a table data cell
-        var content = $(this).text(); //get content of cell
+    $("td").click(function () {
+        var content = $(this).text();
 
-        if (content != "Not Available" && !$(this).is(':first-child')) { //check if content does not contain "Not Available" and not first column
-            $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected
+        if (content != "Not Available" && !$(this).is(':first-child')) {
+            $(this).toggleClass("tdhighlight");
 
-            if ($(this).hasClass("tdhighlight")) { //check if selected cell has class
-                // Get the column index of the clicked cell
+            if ($(this).hasClass("tdhighlight")) {
                 var columnIndex = $(this).index();
-
-                // Get the cliff site name from the header row using eq() method
                 var cliffSite = $('thead th').eq(columnIndex).text();
-
-                // Create display text with styled cliff site
                 var displayText = content + " at <span class='cliff-site'>" + cliffSite + "</span>";
 
-                $('#result').append("<p>"+displayText+"</p>"); //add child element with contents including cliff site
+                $('#result').append("<p>"+displayText+"</p>");
 
-                // Show the modal
                 var modal = new bootstrap.Modal(document.getElementById('activitiesModal'));
                 modal.show();
 
-            } else { //if selected cell doesn't have class
-                // Get the column index and cliff site for removal
+            } else {
                 var columnIndex = $(this).index();
                 var cliffSite = $('thead th').eq(columnIndex).text();
 
-                // Find and remove the specific activity item
                 $('#result p').each(function() {
                     var activityText = $(this).text();
                     var expectedText = content + " at " + cliffSite;
                     if (activityText === expectedText) {
                         $(this).remove();
-                        return false; // Break out of the loop
+                        return false;
                     }
                 });
 
-                // If no more selections, hide the modal if it's open
                 if ($('#result').has('p').length == false) {
                     var modal = bootstrap.Modal.getInstance(document.getElementById('activitiesModal'));
                     if (modal) {
@@ -46,30 +37,25 @@ $(document).ready(function() {
             }
         }
     });
-    // Function to initialize activity cell selection functionality
+
     function initializeActivitySelection() {
-        // Add selectable class and pointer cursor to selectable cells
         $('tbody td').each(function() {
             var cellText = $(this).text().trim().toLowerCase();
-            
-            // Check if cell is not "not available" and not in first column (activity names)
+
             if (!cellText.includes('not available') && !$(this).is(':first-child')) {
                 $(this).addClass('selectable');
             }
         });
-        
-        // Add click event handler for selectable cells
+
         $('.selectable').on('click', function() {
             $(this).toggleClass('selected');
         });
-        
-        // Prevent selection of header cells and first column cells
+
         $('thead th, tbody td:first-child').on('click', function(e) {
             e.preventDefault();
             e.stopPropagation();
         });
     }
-    
-    // Initialize when document is ready
+
     initializeActivitySelection();
 });
--- m2_week3_day2_homework/js/site_scripts.js
+++ m2_week3_day2_homework/js/site_scripts.js
@@ -1,14 +1,13 @@
-var photos = []; // empty array to store image element 
-var fileNames = []; // empty array to store image file names
-var partnerList = []; // empty array to store html list that contain an image
-var image; // empty variable to store the assembled image list codes 
-var openList = "<li class='partner'>"; // variable to store open list tag 
-var closeList = "</li>"; // variable to store close list tag
+var photos = [];
+var fileNames = [];
+var partnerList = [];
+var image;
+var openList = "<li class='partner'>";
+var closeList = "</li>";
 
-// Partner image filenames and alt text
 var partnerFiles = [
     "partner-bustour.png",
-    "partner-cabinrental.png", 
+    "partner-cabinrental.png",
     "partner-campingadv.png",
     "partner-collegetours.png",
     "partner-rentalbike.png",
@@ -18,24 +17,20 @@ var partnerFiles = [
 var partnerAlts = [
     "Partner Bus Tours",
     "Partner Cabin Rental",
-    "Partner Camping Adventure", 
+    "Partner Camping Adventure",
     "Partner College Tours",
     "Partner Rental Bike",
     "Partner Tour Group"
 ];
 
 for(var i=0; i < partnerFiles.length; i++) {
-    // Assemble file name into image element and store in an array 
-    photos.push("<img src='images/partners/" + partnerFiles[i] + "' alt='" + partnerAlts[i] + "'>"); 
-    // Assemble image element from the array with list elements and store in a variable.
+    photos.push("<img src='images/partners/" + partnerFiles[i] + "' alt='" + partnerAlts[i] + "'>");
     image = openList + photos[i] + closeList;
-    partnerList.push(image); // Store the assembled image list code in an array
+    partnerList.push(image);
 }
 
-// Display all six image codes store in the array 
 document.getElementById("partners").innerHTML = partnerList;
 
-// Cliff images data
 var cliffImages = [
     {
         src: "images/cliffs/cliff-west-sea.jpg",
@@ -43,13 +38,13 @@ var cliffImages = [
         description: "West Cliff with ocean view and rock formations"
     },
     {
-        src: "images/cliffs/cliff-north-mountain.jpg", 
+        src: "images/cliffs/cliff-north-mountain.jpg",
         alt: "North Cliff",
         description: "North Cliff with mountain views and rocky ledges"
     },
     {
         src: "images/cliffs/cliff-east-river.jpg",
-        alt: "East Cliff", 
+        alt: "East Cliff",
         description: "East Cliff overlooking a river surrounded by mountains"
     },
     {
@@ -59,9 +54,7 @@ var cliffImages = [
     }
 ];
 
-// Create and append cliff images to lightbox-toggle divs
 $(document).ready(function() {
-    // Add images to the lightbox-toggle divs
     $('.lightbox-toggle').each(function(index) {
         var img = $('<img>').attr({
             'src': cliffImages[index].src,
@@ -71,15 +64,12 @@ $(document).ready(function() {
         $(this).append(img);
     });
 
-    /* Open lightbox on button click */
     $('.lightbox-toggle img').click(function(){
         $('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
         $('.box').fadeIn();
 
-        //Check if lightbox has content and clear it
         $('.box .description, .box .image-container').remove();
 
-        //Get text content in attribute
         var $altvalue = $(this).attr('alt');
         var description = '';
         var imgToClone;
@@ -101,17 +91,14 @@ $(document).ready(function() {
             description = cliffImages[3].description;
         }
 
-        // Create and append description
         var descriptionDiv = $('<div class="description">').text(description);
         $('.box').append(descriptionDiv);
 
-        // Create image container and append cloned image
         var imageContainer = $('<div class="image-container">');
         imageContainer.append(imgToClone);
         $('.box').append(imageContainer);
     });
 
-    /* Click to close lightbox */
     $('.close, .backdrop').click(function(){
         $('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
             $('.backdrop').css('display', 'none');
